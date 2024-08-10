ef1dd6642972380c69cd253b36c77a8d86b364e5
moizsheraz <user146712@gmail.com>
2024-08-10 17:50:38 +0500

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/moizsheraz/TODO
+
+go 1.22.6
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main(){
+	fmt.Println("Hello, World!" + " " + "Greetings, Universe!")
+	var myName string = "John Doe"
+	const mySecondName string = "Smith"
+	
+}
